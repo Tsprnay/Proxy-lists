070f34ca0c0f647855878984b9a17f3a33e4a306
Tsprnay <65571116+Tsprnay@users.noreply.github.com>
2023-12-03 02:50:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,6 @@ import ipaddress
 with open('sites.json') as f:
     sites = json.load(f)
 
-def clean_proxy_format(file_name):
-    with open(file_name, 'r') as f:
-        lines = f.readlines()
-
-    cleaned_lines = []
-    for line in lines:
-        cleaned_line = re.sub(r'^.*://', '', line)
-        cleaned_line = re.sub(r':\d+.*$', '', cleaned_line)
-        cleaned_lines.append(cleaned_line.strip())
-
-    with open(file_name, 'w') as f:
-        f.writelines('\n'.join(cleaned_lines))
-
 def remove_duplicates(file_name):
     lines_seen = set()
     new_file_name = file_name + ".tmp"
@@ -146,11 +133,6 @@ remove_empty_lines('proxies/socks4.txt')
 remove_empty_lines('proxies/socks5.txt')
 remove_empty_lines('proxies/http.txt')
 remove_empty_lines('proxies/https.txt')
-
-clean_proxy_format('proxies/socks4.txt')
-clean_proxy_format('proxies/socks5.txt')
-clean_proxy_format('proxies/http.txt')
-clean_proxy_format('proxies/https.txt')
 
 remove_duplicates('proxies/socks4.txt')
 remove_duplicates('proxies/socks5.txt')
